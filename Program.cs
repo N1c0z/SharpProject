5a934ad5423af466c603f32f193b632135c5cca3
Nico <niccolo.caiti@gmail.com>
2021-04-16 15:53:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HelloFsharp;
+using Quantum.HelloQSharp;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace SharpProject
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string name = "Nico";
+            Console.WriteLine($"Hello World from C# {name}");
+            Say.hello(name);
+            using (var sim = new QuantumSimulator())
+                await HelloW.Run(sim, name);
+        }
+    }
+}
